281979e0b3683ba24c0724b6a76c6f3db69da012
porphyriacus <porph@example.com>
2026-06-29 15:12:37 +0300

    start TaskHub

--- src/TaskManager.API/Hubs/TaskHub.cs
+++ src/TaskManager.API/Hubs/TaskHub.cs
@@ -1,6 +1,22 @@
-﻿namespace TaskManager.API.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+namespace TaskManager.API.Hubs
 {
-    public class TaskHub
+    public class TaskHub : Hub
     {
+        private readonly IProjectRepository _projectRepository;
+        public TaskHub(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            var connectionId = Context.ConnectionId;
+            var userId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var projects = await 
+
+            return base.OnConnectedAsync();
+        }
     }
 }
